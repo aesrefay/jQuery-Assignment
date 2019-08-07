15854edefabe6f5a35651e22a1c49b78c0d19c5f
ahmet esref aydogan <esrefay@ahmets-MacBook.local>
2019-08-07 17:14:07 +0200

    Done

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // Make the project great again with jQuery ..
+$(document).ready(function(){
+  $("#list").on("click",".delete",function(){
+    $(this).parent("li").remove();
+  });
+
+  $("#add-topic").on("submit", function(e){
+    var newtopic = $("#topic-val").val();
+    var newElement = '<li><span class="name">' + newtopic + '</span><span class="delete">delete</span></li>';
+    $("#list").append(newElement);
+    $("#topic-val").val("");
+    e.preventDefault();
+  });
+
+  $("#hide").on("click", function(e){
+    if (!e.target.checked) {
+      $("#list").show();
+    } else {
+      $("#list").hide();
+    }
+  })
+});
